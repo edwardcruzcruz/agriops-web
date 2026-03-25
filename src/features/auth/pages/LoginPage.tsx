313924853b945d01f302d4266595b1c577a1adf5
edwardcruzcruz <edward.cruzcruz@hotmail.com>
2026-03-25 01:31:02 -0500

    feat: improved responsive login styles

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -9,56 +9,58 @@ const LoginPage = () => {
     const {email,password,loading,error,validationErrors,setEmail,setPassword,handleLogin} = useLogin();
     return (
         <div className={styles.container}>
-            <h2 className={styles.title} >LoginPage</h2>
-            {error && (<SharedError 
-                variant='inline'
-                message={error}
-                //onRetry={handleLogin}
-            />)}
-            <form className={styles.form}
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    handleLogin();
-                }}
-                >
-                <div className={styles.formGroup}>
-                    <input
-                        className={styles.input}
-                        type="email"
-                        placeholder='Correo electrónico'
-                        value={email}
-                        onChange={(e)=> setEmail(e.target.value)}
-                        required
-                    />
-                    {validationErrors.email && (
-                        <p className={styles.error}>
-                            {validationErrors.email}
-                        </p>
-                    )}
-                </div>
-                <div className={styles.formGroup}>
-                    <input
-                        className={styles.input}
-                        type="password"
-                        placeholder='Contraseña'
-                        value={password}
-                        onChange={(e)=> setPassword(e.target.value)}
-                        required
-                    />
-                    {validationErrors.password && (
-                        <p className={styles.error}>
-                            {validationErrors.password}
-                        </p>
-                    )}
-                </div>
-                <button type="submit" className={styles.button} disabled={loading} onClick={handleLogin}>
-                    {loading ? <Spinner />: 'Iniciar Sesión'}
-                </button>
-            </form>
-            <p>
-                Don't have an account?{" "}
-                <Link to="/register">Register</Link>
-            </p>
+            <div className={styles.card}>
+                <h3>Sign in</h3>
+                {error && (<SharedError 
+                    variant='inline'
+                    message={error}
+                    //onRetry={handleLogin}
+                />)}
+                <p>
+                    Don't have an account?{" "}
+                    <Link to="/register">Sign up Here</Link>
+                </p>
+                <form className={styles.form}
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleLogin();
+                    }}
+                    >
+                    <div className={styles.formGroup}>
+                        <input
+                            className={styles.input}
+                            type="email"
+                            placeholder='Correo electrónico'
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
+                            required
+                        />
+                        {validationErrors.email && (
+                            <p className={styles.error}>
+                                {validationErrors.email}
+                            </p>
+                        )}
+                    </div>
+                    <div className={styles.formGroup}>
+                        <input
+                            className={styles.input}
+                            type="password"
+                            placeholder='Contraseña'
+                            value={password}
+                            onChange={(e)=> setPassword(e.target.value)}
+                            required
+                        />
+                        {validationErrors.password && (
+                            <p className={styles.error}>
+                                {validationErrors.password}
+                            </p>
+                        )}
+                    </div>
+                    <button type="submit" className={styles.button} disabled={loading} onClick={handleLogin}>
+                        {loading ? <Spinner />: 'Iniciar Sesión'}
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
